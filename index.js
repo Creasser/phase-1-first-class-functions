43a38fdd04088a71a0dd8251edbaa281cc8982d6
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-02 22:52:44 -0500

    commit tree d69606c49a1bedcb6385486ae629f84199c2725d

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(spy){
+    return spy()
+}
+
+function returnsANamedFunction(){
+    return function fn(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
